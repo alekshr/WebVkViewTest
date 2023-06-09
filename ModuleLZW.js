9e26b132a981f203dcfe4c5b32937b272ede89fe
Alexander Shevchenko <alexander.s@danko.team>
2023-06-09 11:57:36 +0700

    Update project

--- ModuleLZW.js
+++ ModuleLZW.js
@@ -2,220 +2,220 @@
 
 export function lzw_encode(s)
 {
-    var dict = {};
-    var data = (s + "").split("");
-    var out = [];
-    var currChar;
-    var phrase = data[0];
-    var code = 256;
-    for (var i = 1; i < data.length; i++)
+  var dict = {};
+  var data = (s + "").split("");
+  var out = [];
+  var currChar;
+  var phrase = data[0];
+  var code = 256;
+  for (var i = 1; i < data.length; i++)
+  {
+    currChar = data[i];
+    if (dict[phrase + currChar] != null)
     {
-        currChar = data[i];
-        if (dict[phrase + currChar] != null)
-        {
-            phrase += currChar;
-        }
-        else
-        {
-            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-            dict[phrase + currChar] = code;
-            code++;
-            phrase = currChar;
-        }
+      phrase += currChar;
     }
-    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-    for (var i = 0; i < out.length; i++)
+    else
     {
-        out[i] = String.fromCharCode(out[i]);
+      out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+      dict[phrase + currChar] = code;
+      code++;
+      phrase = currChar;
     }
-    return out.join("");
+  }
+  out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+  for (var i = 0; i < out.length; i++)
+  {
+    out[i] = String.fromCharCode(out[i]);
+  }
+  return out.join("");
 };
 
 export function lzw_decode(s)
 {
-    var dict = {};
-    var data = (s + "").split("");
-    var currChar = data[0];
-    var oldPhrase = currChar;
-    var out = [currChar];
-    var code = 256;
-    var phrase;
-    for (var i = 1; i < data.length; i++)
+  var dict = {};
+  var data = (s + "").split("");
+  var currChar = data[0];
+  var oldPhrase = currChar;
+  var out = [currChar];
+  var code = 256;
+  var phrase;
+  for (var i = 1; i < data.length; i++)
+  {
+    var currCode = data[i].charCodeAt(0);
+    if (currCode < 256)
     {
-        var currCode = data[i].charCodeAt(0);
-        if (currCode < 256)
-        {
-            phrase = data[i];
-        }
-        else
-        {
-            phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
-        }
-        out.push(phrase);
-        currChar = phrase.charAt(0);
-        dict[code] = oldPhrase + currChar;
-        code++;
-        oldPhrase = phrase;
+      phrase = data[i];
     }
-    return out.join("");
+    else
+    {
+      phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
+    }
+    out.push(phrase);
+    currChar = phrase.charAt(0);
+    dict[code] = oldPhrase + currChar;
+    code++;
+    oldPhrase = phrase;
+  }
+  return out.join("");
 };
 
 
 export function lzw64_encode(s)
 {
-    if (!s) return s;
-    var dict = new Map(); // Use a Map!
-    var data = (s + "").split("");
-    var out = [];
-    var currChar;
-    var phrase = data[0];
-    var code = 256;
-    for (var i = 1; i < data.length; i++)
+  if (!s) return s;
+  var dict = new Map(); // Use a Map!
+  var data = (s + "").split("");
+  var out = [];
+  var currChar;
+  var phrase = data[0];
+  var code = 256;
+  for (var i = 1; i < data.length; i++)
+  {
+    currChar = data[i];
+    if (dict.has(phrase + currChar))
     {
-        currChar = data[i];
-        if (dict.has(phrase + currChar))
-        {
-            phrase += currChar;
-        } else
-        {
-            out.push(phrase.length > 1 ? dict.get(phrase) : phrase.codePointAt(0));
-            dict.set(phrase + currChar, code);
-            code++;
-            if (code === 0xd800) { code = 0xe000; }
-            phrase = currChar;
-        }
-    }
-    out.push(phrase.length > 1 ? dict.get(phrase) : phrase.codePointAt(0));
-    for (var i = 0; i < out.length; i++)
+      phrase += currChar;
+    } else
     {
-        out[i] = String.fromCodePoint(out[i]);
+      out.push(phrase.length > 1 ? dict.get(phrase) : phrase.codePointAt(0));
+      dict.set(phrase + currChar, code);
+      code++;
+      if (code === 0xd800) { code = 0xe000; }
+      phrase = currChar;
     }
-    //console.log ("LZW MAP SIZE", dict.size, out.slice (-50), out.length, out.join("").length);
-    return out.join("");
+  }
+  out.push(phrase.length > 1 ? dict.get(phrase) : phrase.codePointAt(0));
+  for (var i = 0; i < out.length; i++)
+  {
+    out[i] = String.fromCodePoint(out[i]);
+  }
+  //console.log ("LZW MAP SIZE", dict.size, out.slice (-50), out.length, out.join("").length);
+  return out.join("");
 }
 
 export function lzw64_decode(s)
 {
-    var dict = new Map(); // Use a Map!
-    var data = Array.from(s + "");
-    //var data = (s + "").split("");
-    var currChar = data[0];
-    var oldPhrase = currChar;
-    var out = [currChar];
-    var code = 256;
-    var phrase;
-    for (var i = 1; i < data.length; i++)
+  var dict = new Map(); // Use a Map!
+  var data = Array.from(s + "");
+  //var data = (s + "").split("");
+  var currChar = data[0];
+  var oldPhrase = currChar;
+  var out = [currChar];
+  var code = 256;
+  var phrase;
+  for (var i = 1; i < data.length; i++)
+  {
+    var currCode = data[i].codePointAt(0);
+    if (currCode < 256)
     {
-        var currCode = data[i].codePointAt(0);
-        if (currCode < 256)
-        {
-            phrase = data[i];
-        } else
-        {
-            phrase = dict.has(currCode) ? dict.get(currCode) : (oldPhrase + currChar);
-        }
-        out.push(phrase);
-        var cp = phrase.codePointAt(0);
-        currChar = String.fromCodePoint(cp); //phrase.charAt(0);
-        dict.set(code, oldPhrase + currChar);
-        code++;
-        if (code === 0xd800) { code = 0xe000; }
-        oldPhrase = phrase;
+      phrase = data[i];
+    } else
+    {
+      phrase = dict.has(currCode) ? dict.get(currCode) : (oldPhrase + currChar);
     }
-    return out.join("");
+    out.push(phrase);
+    var cp = phrase.codePointAt(0);
+    currChar = String.fromCodePoint(cp); //phrase.charAt(0);
+    dict.set(code, oldPhrase + currChar);
+    code++;
+    if (code === 0xd800) { code = 0xe000; }
+    oldPhrase = phrase;
+  }
+  return out.join("");
 }
 
 export function zip(s)
 {
-    try
+  try
+  {
+    var dict = {}
+    var data = (s + '').split('')
+    var out = []
+    var currChar
+    var phrase = data[0]
+    var code = 256
+    for (var i = 1; i < data.length; i++)
     {
-        var dict = {}
-        var data = (s + '').split('')
-        var out = []
-        var currChar
-        var phrase = data[0]
-        var code = 256
-        for (var i = 1; i < data.length; i++)
-        {
-            currChar = data[i]
-            if (dict[phrase + currChar] != null)
-            {
-                phrase += currChar
-            } else
-            {
-                out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0))
-                dict[phrase + currChar] = code
-                code++
-                phrase = currChar
-            }
-        }
+      currChar = data[i]
+      if (dict[phrase + currChar] != null)
+      {
+        phrase += currChar
+      } else
+      {
         out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0))
-        for (var j = 0; j < out.length; j++)
-        {
-            out[j] = String.fromCharCode(out[j])
-        }
-        return utoa(out.join(''))
-    } catch (e)
-    {
-        console.log('Failed to zip string return empty string', e)
-        return ''
+        dict[phrase + currChar] = code
+        code++
+        phrase = currChar
+      }
     }
+    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0))
+    for (var j = 0; j < out.length; j++)
+    {
+      out[j] = String.fromCharCode(out[j])
+    }
+    return utoa(out.join(''))
+  } catch (e)
+  {
+    console.log('Failed to zip string return empty string', e)
+    return ''
+  }
 }
 
 // Decompress an LZW-encoded base64 string
 export function unzip(base64ZippedString)
 {
-    try
+  try
+  {
+    var s = atou(base64ZippedString)
+    var dict = {}
+    var data = (s + '').split('')
+    var currChar = data[0]
+    var oldPhrase = currChar
+    var out = [currChar]
+    var code = 256
+    var phrase
+    for (var i = 1; i < data.length; i++)
     {
-        var s = atou(base64ZippedString)
-        var dict = {}
-        var data = (s + '').split('')
-        var currChar = data[0]
-        var oldPhrase = currChar
-        var out = [currChar]
-        var code = 256
-        var phrase
-        for (var i = 1; i < data.length; i++)
-        {
-            var currCode = data[i].charCodeAt(0)
-            if (currCode < 256)
-            {
-                phrase = data[i]
-            } else
-            {
-                phrase = dict[currCode] ? dict[currCode] : oldPhrase + currChar
-            }
-            out.push(phrase)
-            currChar = phrase.charAt(0)
-            dict[code] = oldPhrase + currChar
-            code++
-            oldPhrase = phrase
-        }
-        return out.join('')
-    } catch (e)
-    {
-        console.log('Failed to unzip string return empty string', e)
-        return ''
+      var currCode = data[i].charCodeAt(0)
+      if (currCode < 256)
+      {
+        phrase = data[i]
+      } else
+      {
+        phrase = dict[currCode] ? dict[currCode] : oldPhrase + currChar
+      }
+      out.push(phrase)
+      currChar = phrase.charAt(0)
+      dict[code] = oldPhrase + currChar
+      code++
+      oldPhrase = phrase
     }
+    return out.join('')
+  } catch (e)
+  {
+    console.log('Failed to unzip string return empty string', e)
+    return ''
+  }
 }
 
 // ucs-2 string to base64 encoded ascii
 function utoa(str)
 {
-    return window.btoa(unescape(encodeURIComponent(str)))
+  return window.btoa(unescape(encodeURIComponent(str)))
 }
 // base64 encoded ascii to ucs-2 string
 function atou(str)
 {
-    return decodeURIComponent(escape(window.atob(str)))
+  return decodeURIComponent(escape(window.atob(str)))
 }
 
 
 
 export function decodeHtmlEntity(str)
 {
-    return str.replace(/&#(\d+);/g, function (match, dec)
-    {
-        return String.fromCharCode(dec);
-    });
+  return str.replace(/&#(\d+);/g, function (match, dec)
+  {
+    return String.fromCharCode(dec);
+  });
 };
--- ModuleVk.js
+++ ModuleVk.js
@@ -2,85 +2,73 @@ import { myGameInstance } from './ModuleGame.js'
 import { Sleep } from './ModuleSleep.js'
 import { lzw_encode, lzw_decode, decodeHtmlEntity } from './ModuleLZW.js'
 
+
 let loadData = "";
 let isSaveData = false;
 let accessToken;
-let appId = 51593514;
+
+const vkAppId = 51593514;
+
 const timeWaitSave = 3000;
 
-
-// fetch("https://api.vk.com/method/storage.set", {
-//                     method: "POST",
-//                     mode: "no-cors",
-//                     credentials: "include",
-//                     headers:
-//                     {
-//                         "Content-Type": "multipart/form-data",
-//                         "Acecess-Control-Allow-Origin": "*",
-//                         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-//                         "Access-Control-Allow-Headers": "Content-Type"
-//                     },
-//                     body: JSON.stringify({key: "MyLocalKey", value: "GSGDSG", access_toke: accessToken, v: "5.131"})
-//                 })
-
-
-export async function InitVk() {
+export async function InitVk()
+{
     await vkBridge.subscribe((e) => console.log("vkBridge event", e));
-    await vkBridge.subscribe(function (e) {
-        if (e.detail.type === "VKWebAppShowNativeAdsResult") {
+    await vkBridge.subscribe(function (e)
+    {
+        if (e.detail.type === "VKWebAppShowNativeAdsResult")
+        {
             console.log(`Реклама показана e.detail.data = ${e.detail.data}`);
         }
 
-        if (e.detail.type === "VKWebAppShowNativeAdsFailed") {
+        if (e.detail.type === "VKWebAppShowNativeAdsFailed")
+        {
             console.log(`Реклама была не показана из-за ошибки: e.detail.data = ${e.detail.data}`);
-        }
-
-        if (e.detail.type === "VKWebAppCloseResult") {
-            console.log(`Закрыли приложение`);
         }
     });
 
     let data = await vkBridge.send("VKWebAppInit", {});
-
-    let dataGetAccessToken = await vkBridge.send('VKWebAppGetAuthToken', { app_id: appId, scope: '' });
+    let dataGetAccessToken = await vkBridge.send('VKWebAppGetAuthToken', { app_id: vkAppId, scope: '' });
     accessToken = dataGetAccessToken.access_token;
-    console.log(`accessToken = ${accessToken}`);
-    if (data.result) {
+
+    if (data.result)
+    {
         console.log("Is init SDK VK");
         await InitLoadData();
+        window.addEventListener('unload', (event) => myGameInstance.SendMessage("WebDataManager", "SaveByExit"));
 
-        // document.addEventListener('visibilitychange', function () {
-        //     if (document.hidden) {
-        //         myGameInstance.SendMessage("WebDataManager", "SaveByExit");
-        //     }
-        // });
-    } else {
+    } else
+    {
         console.log("Is not inited SDK VK");
     }
 }
 
-export async function VkPreloadReward() {
+export async function VkPreloadReward()
+{
     await VkActionAds("VKWebAppCheckNativeAds",
         'VkRewardAds',
         'reward',
         'PreloadAdsResult');
 }
 
-export async function VkPreloadInterstitial() {
+export async function VkPreloadInterstitial()
+{
     await VkActionAds("VKWebAppCheckNativeAds",
         'VkInterstitialAds',
         'interstitial',
         'PreloadAdsResult');
 }
 
-export async function VkShowReward() {
+export async function VkShowReward()
+{
     await VkActionAds("VKWebAppShowNativeAds",
         'VkRewardAds',
         'reward',
         'ResultShowAds');
 }
 
-export async function VkShowInterstitial() {
+export async function VkShowInterstitial()
+{
     await VkActionAds("VKWebAppShowNativeAds",
         'VkInterstitialAds',
         'interstitial',
@@ -90,29 +78,36 @@ export async function VkShowInterstitial() {
 async function VkActionAds(nameApiMethod,
     nameGameObjectUnity,
     typeAds,
-    callBackResult) {
-    try {
+    callBackResult)
+{
+    try
+    {
         let data = await vkBridge.send(nameApiMethod, { ad_format: typeAds });
-        if (data.result) {
+        if (data.result)
+        {
             myGameInstance.SendMessage(nameGameObjectUnity, callBackResult, 1);
-        } else {
+        } else
+        {
             myGameInstance.SendMessage(nameGameObjectUnity, callBackResult, 0);
         }
     }
-    catch (e) {
+    catch (e)
+    {
         myGameInstance.SendMessage(nameGameObjectUnity, callBackResult, 0);
         console.log(`Vk Action e = ${JSON.stringify(e)}`);
     }
 }
 
-export function SetIFrameSize() {
+export function SetIFrameSize()
+{
     vkBridge.send('VKWebAppResizeWindow', {
         width: document.documentElement.clientWidth,
         height: document.documentElement.clientHeight
     });
 }
 
-export function VkLoadCloud() {
+export function VkLoadCloud()
+{
     Sleep(timeWaitSave);
     let len = 1024;
     let lenData = loadData.length;
@@ -120,7 +115,8 @@ export function VkLoadCloud() {
     let size = 0;
     let sliceStr;
 
-    while (lenData > 0) {
+    while (lenData > 0)
+    {
         sliceStr = loadData.slice(size, size + len);
         myGameInstance.SendMessage('WebDataManager', 'InitData', sliceStr);
         size = size + len;
@@ -131,10 +127,12 @@ export function VkLoadCloud() {
     return true;
 }
 
-export async function VkSaveCloud(jsonData, flush) {
-    try {
-        if (isSaveData) {
-            console.log("Сохраняем");
+export async function VkSaveCloud(jsonData, flush)
+{
+    try
+    {
+        if (isSaveData)
+        {
             let len = 2048;
             let lenJsonData = String(jsonData.length);
             let encodeJson = lzw_encode(jsonData);
@@ -142,74 +140,70 @@ export async function VkSaveCloud(jsonData, flush) {
             let size = 0;
             let sliceStr;
             let index = 0;
-            while (lenData > 0) {
+
+            while (lenData > 0)
+            {
                 sliceStr = encodeJson.slice(size, size + len);
                 size = size + len;
                 lenData = lenData - len;
+
+
                 let formData = new FormData();
                 formData.append("key", "SavedStringKey" + String(index));
                 formData.append("value", sliceStr);
-                formData.append("access_toke", accessToken);
+                formData.append("access_token", accessToken);
                 formData.append("v", "5.131");
 
+                let isRequestBeacon = navigator.sendBeacon(`https://api.vk.com/method/storage.set`, formData);
 
-                await fetch(`https://api.vk.com/method/storage.set?key=SavedStringKey${index}&value=${sliceStr}&access_token=${accessToken}&v=${5.131}`, 
-                {
-                    method: "GET",
-                    credentials: "include",
-                    headers:
-                    {
-                        "Content-Type": "multipart/form-data",
-                        "Acecess-Control-Allow-Origin": "*",
-                        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-                        "Access-Control-Allow-Headers": "Content-Type"
-                    },
-                });
+                console.log(`Отправили ан секрвер вк = ${isRequestBeacon}`);
+
+                localStorage.setItem("unloadSaveCloud", false);
                 index++;
             }
 
-            let formDataLen = new FormData();
-            formDataLen.append("key", "LenJsonData");
-            formDataLen.append("value", lenJsonData);
-            formDataLen.append("access_toke", accessToken);
-            formDataLen.append("v", "5.131");
+            let formData = new FormData();
+            formData.append("key", "LenJsonData");
+            formData.append("value", lenJsonData);
+            formData.append("access_token", accessToken);
+            formData.append("v", "5.131");
 
-            await fetch(`https://api.vk.com/method/storage.set?key=LenJsonData&value=${lenJsonData}&access_token=${accessToken}&v=${5.131}`, {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-                    "Access-Control-Allow-Headers": "Content-Type"
-                },
-            });
+            navigator.sendBeacon(`https://api.vk.com/method/storage.set`, formData);
+            localStorage.setItem("unloadSaveCloud", false);
         }
     }
-    catch (e) {
+    catch (e)
+    {
         console.log(`save exception Message: ${e.message}`)
     }
 }
 
-export async function VkInviteFriends() {
-    try {
+
+export async function VkInviteFriends()
+{
+    try
+    {
         await vkBridge.send('VKWebAppShowInviteBox', {});
     }
-    catch (e) {
+    catch (e)
+    {
         console.log(e);
     }
 }
 
 
-async function InitLoadData() {
+async function InitLoadData()
+{
     let keysLoad = await vkBridge.send('VKWebAppStorageGetKeys', { count: 10, offset: 0 });
     let lenValueJsonData = await vkBridge.send('VKWebAppStorageGet', { keys: ['LenJsonData'] });
     let dataKeys = await vkBridge.send('VKWebAppStorageGet', {
         keys: keysLoad.keys
     });
 
-    for (let index = 0; index < keysLoad.keys.length; index++) {
-        if (dataKeys.keys[index].key.indexOf("SavedStringKey") >= 0) {
+    for (let index = 0; index < keysLoad.keys.length; index++)
+    {
+        if (dataKeys.keys[index].key.indexOf("SavedStringKey") >= 0)
+        {
             loadData += dataKeys.keys[index].value;
         }
     }
