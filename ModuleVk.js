5e61c8b3897894af17bb96cbb447ada4c3652091
Alexander Shevchenko <Shevchenko.A.Sr@yandex.ru>
2023-07-04 11:50:27 +0700

    Update ModuleVk.js

--- ModuleVk.js
+++ ModuleVk.js
@@ -59,6 +59,7 @@ export async function InitVk() {
 }
 
 function simulateTouchOnElement(element, screenX, screenY) {
+    console.log("Вызвали touch");
     var touchEvent = new TouchEvent('touchstart', {
       bubbles: true,
       cancelable: true,
