9650237db7948f8e88a7f8f3aa65e493ca953603
Alexander Shevchenko <Shevchenko.A.Sr@yandex.ru>
2023-07-04 11:04:29 +0700

    Update ModuleVk.js

--- ModuleVk.js
+++ ModuleVk.js
@@ -48,7 +48,6 @@ export async function InitVk() {
         document.querySelector("body").onclick = () => console.log(`Кликнули на тело сайта`);
         document.addEventListener("visibilitychange", function(){
             if(document.visibilityState === "visible"){
-                Sleep(1000);
                 document.querySelector("body").dispatchEvent(new Event("click"));
             }
           });
